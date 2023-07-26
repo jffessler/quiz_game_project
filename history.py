506b4dd0dbc6bb86a2582b4c8e71513a78e8d66f
John Fessler <jffessler@gmail.com>
2023-07-25 22:34:31 -0700

    Revert to 99aaa29

--- history.py
+++ history.py
@@ -1,9 +1,9 @@
-history_questions = {
+question_library = {
     "The United States bought Alaska from which country? ": "Russia",
     "Who was the fourth president of the United States? ": "James Madison",
     "Which era marked a switch from agricultural practices to industrial practices? ": "The Industrial Revolution",
     "What was the name of the series of programs and projects President Franklin D. Roosevelt enacted during The Great Depression? ": "The New Deal",
     "Fill in the blank: The 19th Amendment guarantees ____ the right to vote ": "Women"
 }
-questions = list(history_questions.keys())
+questions = list(question_library.keys())
 # print(questions)
--- quiz_game_project.py
+++ quiz_game_project.py
@@ -1,9 +1,10 @@
 def quiz_game():
+    import random
     # Welcome to the quiz game! 
     print("Welcome to the quiz game!")
 
     # categories of quiz topics
-    categories = ["computers", "animals", "history"]
+    categories = ["Computers", "Animals", "History"]
     #while loops for error handling the choices of whether to play and the category to play
     while True:
         playing = input("Would you like to play a game? (Y/N) ")
@@ -16,20 +17,41 @@ def quiz_game():
             break
     while playing.lower() == "y":
         category = input("Choose your category: computers, animals, or history: ")
-        if category not in categories:
+        if category.capitalize() not in categories:
             print("Sorry, invalid input")
             continue
         else:
             break
 
-    #importing the questions of the chosen category
+    #importing and storing the questions of the chosen category
     cat_chosen = __import__(category)
-    print(cat_chosen.questions)
+    questions = cat_chosen.questions
+    library = cat_chosen.question_library
 
     # The game code
     while playing.lower() == "y":
-        print("Best of luck!")
+        #initialization
+        print(f"Best of luck on five trivia questions on {category.capitalize()}!")
+        score = 0
+        i = 5
+        questions_ask = []
+        #questions
+        for num in range(i):
+            question_num = random.randint(0,len(questions)-1)
+            while question_num in questions_ask:
+                question_num = random.randint(0,len(questions)-1)
+            print(questions[question_num])
+            answer = input("Answer: ")
+            if library[questions[question_num]] == answer.capitalize():
+                print("Great Job!")
+                score += 1
+                print(f"Your score is {score}")
+            else:
+                print(f"Sorry, wrong answer, the correct answer is {library[questions[question_num]]}")
+            questions_ask.append(question_num)
+            print(questions_ask)
 
+        
         while True:
             playing = input("would you like to play again? (y/n) ")
             if not playing.lower() == "y" and not playing.lower() == "n":
